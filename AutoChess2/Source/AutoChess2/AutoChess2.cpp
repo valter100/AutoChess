b75bb4def1cc201497ecf06af9a664f179f57a16
valter100 <va6648li-s@student.lu.se>
2023-10-23 12:06:11 +0200

    Project

---
+++ AutoChess2/Source/AutoChess2/AutoChess2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AutoChess2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AutoChess2, "AutoChess2" );
